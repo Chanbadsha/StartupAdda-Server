be223f5445510974a67413c19859856d0ee0aeb6
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-05-20 06:35:54 +0600

    Coonnect to data base and implemenet get all ideass and get single ideas by id

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const url = process.env.MONGODB_URL || "";
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the startup-adda backend");
 });
+
+// Mongodb connect
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Data Base
+    const startupDataBase = client.db("StartUpAdda");
+    const StartUpCollection = startupDataBase.collection("StartUpCollection");
+
+    // Crude Operation
+
+    // Get ALl Startup Ideas
+    app.get("/ideas", async (req, res) => {
+      const ideas = await StartUpCollection.find({}).toArray();
+      res.json(ideas);
+    });
+    // Get Single Startup Ideas By Id
+    app.get("/ideas/:ideasId", async (req, res) => {
+      const { ideasId } = await req.params;
+      const filter = {
+        _id: new ObjectId(ideasId),
+      };
+      const ideas = await StartUpCollection.findOne(filter);
+      res.json(ideas);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`The startup adda server is running on port ${port}`);
